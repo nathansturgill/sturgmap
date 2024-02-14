8f05247268ff41b8220183589f710f71a253b4b7
nathan sturgill <nsturgi1@vols.utk.edu>
2024-02-14 14:26:07 -0500

    creating random numbers in the code from common.py

--- sturgmap/common.py
+++ sturgmap/common.py
@@ -5,3 +5,12 @@ def hello_world():
     """Prints "Hello World!" to the console.
     """
     print("Hello World!")
+
+
+    def random_number()
+    import random
+    return random.random()
+
+
+
+        
