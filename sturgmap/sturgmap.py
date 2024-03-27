eeff7331c2a040cba3129d5542b59a2587c7fafb
nathan sturgill <nsturgi1@vols.utk.edu>
2024-03-27 16:13:10 -0400

    added docs strings for documentation

--- sturgmap/sturgmap.py
+++ sturgmap/sturgmap.py
@@ -3,8 +3,18 @@
 import ipyleaflet
 
 class Map(ipyleaflet.Map):
+    """_summary_
 
+    Args:
+        ipyleaflet (_type_): _description_
+    """
     def __init__(self, center=[20, 0], zoom=2, **kwargs):
+        """_summary_
+
+        Args:
+            center (list, optional): _description_. Defaults to [20, 0].
+            zoom (int, optional): _description_. Defaults to 2.
+        """
         super().__init__(center=center, zoom=zoom, **kwargs)
         self.add_control(ipyleaflet.LayersControl())
     
