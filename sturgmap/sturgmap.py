a2fd5b121dc1313a6f58feec4af3181c638fe5bd
nathan sturgill <nsturgi1@vols.utk.edu>
2024-03-27 13:51:44 -0400

    updated sturmap.py and mkdocs file

--- sturgmap/sturgmap.py
+++ sturgmap/sturgmap.py
@@ -4,7 +4,10 @@ import ipyleaflet
 
 class Map(ipyleaflet.Map):
 
-    def __init__(self, **kwargs):
-        super().__init__(**kwargs)
-        self .add_control(ipyleaflet.LayersControl())
-        
+    def __init__(self, center=[20, 0], zoom=2, **kwargs):
+        super().__init__(center=center, zoom=zoom, **kwargs)
+        self.add_control(ipyleaflet.LayersControl())
+    
+    def add_tile_layer(self, url, name, **kwargs):
+        layer= ipyleaflet.TileLayer(url=url, name=name, **kwargs)
+        self.add(layer)
