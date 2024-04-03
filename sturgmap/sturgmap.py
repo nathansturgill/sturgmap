90c812fa04042f6ff270ecd63083697e61cc8bab
nathan sturgill <nsturgi1@vols.utk.edu>
2024-04-03 13:36:36 -0400

    updated basemap tool

--- sturgmap/sturgmap.py
+++ sturgmap/sturgmap.py
@@ -1,6 +1,7 @@
 """Main module."""
 
 import ipyleaflet
+from ipyleaflet import basemaps
 
 class Map(ipyleaflet.Map):
     """_summary_
@@ -21,4 +22,10 @@ class Map(ipyleaflet.Map):
     def add_tile_layer(self, url, name, **kwargs):
         layer= ipyleaflet.TileLayer(url=url, name=name, **kwargs)
         self.add(layer)
-            
+
+    def add_basemap(self, name):
+        if isinstance(name, str):
+            url = eval(f"basemaps.{name}").build_url()
+            self.add_tile_layer(url, name)
+        else:
+            self.add_name()
